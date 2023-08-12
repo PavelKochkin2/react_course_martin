51c32b370e83a7de098561281feba411511b2b31
Pavel Kochkin <pavel.kochkin.ext@publicismedia.com>
2023-08-12 12:28:11 +0200

    feat: implemented year filter

--- src/App.js
+++ src/App.js
@@ -3,38 +3,45 @@ import NewExpense from "./components/NewExpense";
 import Expenses from "./components/Expenses";
 import { useState } from "react";
 
+const defaultState = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [appState, setAppState] = useState(defaultState);
 
   const onNewExpenseSubmit = (event, state) => {
     event.preventDefault();
-    console.log({ ...state, id: Math.random().toString() });
+
+    const newItem = { ...state, id: Math.random().toString() };
+
+    setAppState((prevState) => {
+      return [newItem, ...prevState];
+    });
   };
 
   return (
     <div>
       <NewExpense onNewExpenseSubmit={onNewExpenseSubmit} />
-      <Expenses expenses={expenses} />
+      <Expenses expenses={appState} />
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,8 +5,22 @@ import { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 export default function Expenses({ expenses }) {
   const [dateFilterValue, setDateFilterValue] = useState();
+
+  const incomingExpenses = expenses;
+
+  const [filteredExpenses, setfilteredExpenses] = useState(expenses);
+
   const onFilterChanged = (event) => {
     setDateFilterValue(event.target.value);
+    filterExpenses(event.target.value);
+  };
+
+  const filterExpenses = (selectedYear) => {
+    const filteredExpenses = incomingExpenses.filter(
+      (ex) => ex.date.toISOString().substring(0, 4) === selectedYear
+    );
+
+    setfilteredExpenses(filteredExpenses);
   };
 
   return (
@@ -16,10 +30,15 @@ export default function Expenses({ expenses }) {
         selectedYear={dateFilterValue}
       />
       <div>Picked date in the filter: {dateFilterValue}</div>
-      {expenses.map((ex) => {
+      {filteredExpenses.map((ex) => {
         return (
           <Card>
-            <ExpenseItem title={ex.title} amount={ex.amount} date={ex.date} />
+            <ExpenseItem
+              title={ex.title}
+              amount={ex.amount}
+              date={ex.date}
+              key={ex.id}
+            />
           </Card>
         );
       })}
